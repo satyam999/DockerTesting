e33e4f06ab48eac3298ee2b549d7986fe09c20e9
Durgesh Srivastav <durgeshnandani999@gmail.com>
2026-05-31 21:37:36 +0530

    First Commit

---
+++ src/main/java/com/dops/docker/DockerTesting1Application.java
@@ -0,0 +1,20 @@
+package com.dops.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerTesting1Application {
+
+	@GetMapping("/dockerMessgae")
+	public String getDockerMessgae() {
+		return "Docker Testing";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTesting1Application.class, args);
+	}
+
+}
